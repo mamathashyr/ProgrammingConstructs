906401037643423f07b2b7d32de6666a0483a464
mamathashyr <89257950+mamathashyr@users.noreply.github.com>
2021-08-25 16:58:14 +0530

    Update arith.sh

--- arith.sh
+++ arith.sh
@@ -1,15 +1,36 @@
-#!/bin/bash
-a=2
-b=5
-c=7
-for input in $a $b $c
-do
-if [[ $input -eq 2 ]]
-then
-echo "a: $a"
-elif  [[ $input -eq 5 ]]
-then echo "b: $b"
-else 
-echo "c: $c"
-fi
-done
+#! /bin/bash
+
+#UC: 
+a=20
+b=30
+c=40
+
+#UC2:
+
+comp1=(a+b*c)
+echo $comp1
+
+#UC3:
+
+comp2=(a*b+c)
+echo $comp2 
+
+#UC4:
+
+comp3=(c+a/b)
+echo $comp3
+
+#UC5:
+
+comp4=(a%b+c)
+echo $comp4
+
+#UC6:
+
+declare -A store
+
+store=(comp1 comp2 comp3 comp4)
+
+#UC7:
+
+echo ${store[@]}
